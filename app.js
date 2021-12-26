05e598e8e37dcdf853aa7bb36f13911ef74512e3
Gaurav Lathwal <gouravlathwal63@gmail.com>
2021-12-26 18:56:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const { SocketAddress } = require('net')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+const users = {};
+const sid = {};
+const uid = {};
+io.on('connection', socket => {
+    const mobile = socket.handshake.query.mobile;
+    sid[mobile] = socket.id;
+    users[socket.id] = mobile;
+    console.log(mobile);
+
+
+    socket.on('join-user', (userId) => {
+        uid[users[socket.id]] = userId;
+
+    })
+
+    socket.on("call-to", mobile => {
+        const user = users[socket.id];
+        console.log(user, mobile);
+        socket.to(sid[mobile]).emit("new-call", uid[user], user);
+    })
+    socket.on("call-declined", mobile => {
+        socket.to(sid[mobile]).emit("call-rejected", users[socket.id] + " declined your call");
+    })
+
+    socket.on('disconnect', () => {
+        let user = users[socket.id];
+        socket.broadcast.emit('user-disconnected', uid[user]);
+    })
+
+    socket.on("my-id", data => {
+        console.log(socket.id);
+    })
+})
+
+server.listen(3000); 
---
+++ public/script.js
@@ -0,0 +1,82 @@
+const mobile = prompt('What is your mobile?');
+const socket = io('/', { query: "mobile=" + mobile })
+
+
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        console.log(userId);
+        connectToNewUser(userId, stream)
+    })
+    socket.on("new-call", (userId, mobile) => {
+        console.log("new call from: ", mobile);
+        if (confirm("Call from " + mobile + ". Press ok for Accept."))
+            connectToNewUser(userId, stream)
+        else {
+            socket.emit("call-declined", mobile);
+        }
+
+    })
+})
+socket.on("call-rejected", message => {
+    console.log(message);
+})
+
+
+function call() {
+    let mobile = document.getElementById("mobile").value;
+    socket.emit("call-to", mobile);
+}
+
+socket.on('user-disconnected', userId => {
+    console.log(userId);
+    if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+    console.log("connected", id);
+    socket.emit('join-user', id);
+})
+
+function connectToNewUser(userId, stream) {
+    console.log("hello");
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        console.log("yes");
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
